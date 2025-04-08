6e5ee912a6d206a7a6cdf03cc48b259b21282c1f
JayGovinda13 <jaygovinda13@gmail.com>
2025-04-08 16:44:01 -0300

    body2 wip

--- dr-marilia/src/App.jsx
+++ dr-marilia/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import { Button1, Button2, Button3, Button4 } from './Components/Buttons/Buttons'
+import Body2 from './Components/Body2/Body2'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button1/>
-      <Button2/>
-      <Button3/>
-      <Button4/>
+      <Body2 />
     </>
   )
 }
